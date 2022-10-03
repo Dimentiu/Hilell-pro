d4ebf563c972f1a42b6e50b26457b22833cb32a4
Dimentiu <dimentiu4@gmail.com>
2022-10-03 16:26:07 +0300

    DZ1

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -1,5 +1,3 @@
-from http.cookies import SimpleCookie
-
 def parse(query: str) -> dict:
     return {}
 
@@ -13,10 +11,7 @@ if __name__ == '__main__':
 
 
 def parse_cookie(query: str) -> dict:
-    cookie = SimpleCookie()
-    cookie.load(query)
-    res = {k: v.value for k, v in cookie.items()}
-    return res
+    return {}
 
 
 if __name__ == '__main__':
@@ -24,13 +19,3 @@ if __name__ == '__main__':
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('Home=door;') == {'Home': 'door'}
-    assert parse_cookie('Home=door=window;size=28;') == {'Home': 'door=window', 'size': '28'}
-    assert parse_cookie('name=!!!;') == {'name': '!!!'}
-    assert parse_cookie('dog=Mars;') == {'dog': 'Mars'}
-    assert parse_cookie('car=bmw;') == {'car': 'bmw'}
-    assert parse_cookie('size=45;') == {'size': '45'}
-    assert parse_cookie('color=red;') == {'color': 'red'}
-    assert parse_cookie('symbol=~~!!') == {'symbol': '~~!!'}
-    assert parse_cookie('~~=!!;') == {'~~': '!!'}
-    assert parse_cookie('cat=Tom;') == {'cat': 'Tom'}
