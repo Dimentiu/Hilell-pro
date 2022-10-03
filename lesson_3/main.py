3148d596c5b21fd00fabadeac53de0ba2560469e
Dimentiu <dimentiu4@gmail.com>
2022-10-03 20:32:32 +0300

    part parse full

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -1,5 +1,11 @@
+from urllib import parse as p
+
+
 def parse(query: str) -> dict:
-    return {}
+    p.urlsplit(query)
+    p.parse_qs(p.urlsplit(query).query)
+    result = dict(p.parse_qsl(p.urlsplit(query).query))
+    return result 
 
 
 if __name__ == '__main__':
@@ -8,6 +14,16 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('https://example.com/path/to/page?car=bmw&color=red') == {'car': 'bmw', 'color': 'red'}
+    assert parse('https://example.com/path/to/page?size=12&color=purple') ==  {'size': '12', 'color': 'purple'}
+    assert parse('https://example.com/path/to/page?file=home&size=12') == {'file': 'home', 'size': '12'}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple')
+    assert parse('https://example.com/path/to/page?name=!&12=!!') == {'name': '!', '12': '!!'}
+    assert parse('https://example.com/path/to/page?**=12&!!=~~') == {'**': '12', '!!': '~~'}
+    assert parse('https://example.com/path/to/page?q=w&e=r') == {'q': 'w', 'e': 'r'}
+    assert parse('https://example.com/path/to/page?дом=дверь&стол=тарелка') == {'дом': 'дверь', 'стол': 'тарелка'}
+    assert parse('https://example.com/path/to/page?Тима=Тома&Тима=Тома') == {'Тима': 'Тома', 'Тима': 'Тома'}
+    assert parse('https://example.com/path/to/page?=&') == {}
 
 
 def parse_cookie(query: str) -> dict:
