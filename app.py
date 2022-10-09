df452c7d5a7ff9435239989cf565645078989814
Dimentiu <dimentiu4@gmail.com>
2022-10-09 13:59:04 +0300

    dz-2.2

--- app.py
+++ app.py
@@ -9,16 +9,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def red_method():
-    """Function red txt file
-    """
-    red_path = os.path.join(os.getcwd(), "requirements.txt")
-    with open(red_path) as f:
-        data = f.readlines()
-        return data
+    pass
 
 
 def user_data_method():
-    pass
+    """Function generate 100 random users"""
+    for i in range(101):
+        name = fake.name()
+        email = fake.email()
+        response = {
+            "name": name,
+            "email": email
+        }
+    return (response)
 
 
 def red_csv_method():
