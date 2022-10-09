1a23995fde8408bdd1a03305373130bc289ab4e5
Dimentiu <dimentiu4@gmail.com>
2022-10-09 14:56:13 +0300

    dz-2.4

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from faker import Faker
+import requests
 
 fake = Faker()
 
@@ -9,12 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def red_method():
-    """Function red txt file
-    """
-    red_path = os.path.join(os.getcwd(), "requirements.txt")
-    with open(red_path) as f:
-        data = f.readlines()
-        return data
+    pass
 
 
 def user_data_method():
@@ -26,4 +22,25 @@ def red_csv_method():
 
 
 def astros_method():
-    pass
+    """
+    Function make a request to the URL and create html
+    templates with astronaut list
+    """
+    url = 'http://api.open-notify.org/astros.json'
+    responseJSON = requests.get(url).json()
+    astros = """
+        <h1>List astronauts</h1>
+        <h2>There are {astronauts_count} astronauts in orbit</h2>
+        <ul>
+            {astronauts_list}
+        </ul>
+        <a href="/">Back</a>
+        """
+    astronauts_list = ''
+    for astronaut in responseJSON.get('people'):
+        astronauts_list += "<li>{astronaut_name}</li>".format(
+                            astronaut_name=astronaut.get("name"),
+                            )
+    return astros.format(
+        astronauts_list=astronauts_list,
+        astronauts_count=responseJSON.get('number'))
