e83781c293438704e91288415e471c7f58bee075
Dimentiu <dimentiu4@gmail.com>
2022-10-09 13:49:15 +0300

    dz-2.1

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 import os
 from flask import Flask
+from faker import Faker
+
+fake = Faker()
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def red_method():
+    """Function red txt file
+    """
     red_path = os.path.join(os.getcwd(), "requirements.txt")
     with open(red_path) as f:
         data = f.readlines()
         return data
+
 
 def user_data_method():
     pass
