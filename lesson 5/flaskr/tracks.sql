f010b8d83cfd3ad38fd3940320f8cca8f8b709d2
Dimentiu <dimentiu4@gmail.com>
2022-10-12 09:57:19 +0300

    Flask, Sqlite

---
+++ lesson 5/flaskr/tracks.sql
@@ -0,0 +1,7 @@
+INSERT INTO tracks (id, title, artist, genre, length)
+VALUES
+  ('1', 'Nothing Else Matters', 'Metallica', 'metal', '388'),
+  ('2', 'The Unforgiven', 'Metallica', 'metal', '389'),
+  ('3', 'Enter Sandman', 'Metallica', 'metal', '332'), 
+  ('4', 'Highway To Hell', 'AC-DC', 'rock', '178'),
+  ('5', 'Back In Black', 'AC-DC', 'rock', '255');
